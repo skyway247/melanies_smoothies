984b3f9bf100e18512b792608339a4cebd23a1fa
Akash Verma <108573198+skyway247@users.noreply.github.com>
2024-09-11 01:46:04 +0530

    Update streamlit_app.py
    json

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,6 +47,11 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 st.text(fruityvice_response)
 
+import requests
+fruityvice_response = requests.get ("https://fruityvice.com/api/fruit/watermelon")
+# st.text(fruityvice_response.json())
+fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
 
         
 
